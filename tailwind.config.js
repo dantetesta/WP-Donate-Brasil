f7ce1003f663bbbbbda8ab2069e6e287c8966bd2
dantetesta <suporte@dantetesta.com.br>
2025-12-23 19:27:37 -0300

    feat: Tailwind CSS compilado localmente (40KB)
    - Removido CDN do Tailwind CSS
    - Adicionado tailwind.min.css compilado localmente (40KB)
    - Configurado tailwind.config.js para escanear arquivos PHP/JS
    - Scripts npm para recompilação: npm run build / npm run watch
    - Adicionado .gitignore para node_modules e arquivos de build
    
    Plugin agora 100% compatível com WordPress.org (sem CDNs externos)
    
    Autor: Dante Testa (https://dantetesta.com.br) - 23/12/2024

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.php",
+    "./admin/**/*.php",
+    "./includes/**/*.php",
+    "./public/**/*.php",
+    "./admin/js/*.js",
+    "./public/js/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
